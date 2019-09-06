9efc9f0edeeacc6cee560b7e342d091177d07aba
Kelly M <moreira.kd@outlook.com>
2019-09-06 11:35:28 -0400

    fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      user: {},
+      followers: []
+    };
+  }
+
+  componentDidMount() {
+    console.log("first render - DidMount")
+
+    //fetch
+    fetch('http://localhost:5000/api/players')
+      .then(res => res.json())
+      .then(data => this.setState({user: data}));
+
+      
+  }
+
+  componentDidUpdate() {
+    console.log("runs on every rerender -DidUpdate", this.state);
+  }
+
+
+  render() {
+    return(
+      <div className="App">
+      
+      </div>
+    )
+  }
+
+  
 }
+
+
+
 
 export default App;
